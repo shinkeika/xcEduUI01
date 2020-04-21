c3a9ed68ad6d10bc6a0218492e16d7f4affe1b62
shinkeika <517495546@qq.com>
2020-04-21 11:17:24 +0800

    first

---
+++ xc-ui-pc-teach/config/sysConfig.js
@@ -0,0 +1,11 @@
+var sysConfig = {
+    xcApiUrlPre: '/api',
+    //xcApiUrlPre: '',
+    xcApiUrl: 'http://api.xuecheng.com',
+    imgUrl:'http://www.imgxuecheng.com/',
+    videoUrl:'http://video.xuecheng.com',
+    openAuthenticate:true,
+    openAuthorize:false
+}
+
+module.exports = sysConfig
